b7bac1346043200f4fccaf8c52bc3b476debaffd
jrasor <jrasor@boscotech.edu>
2020-12-31 07:11:11 -0800

    Wobbler detection added to Pullbot. Does not compile yet.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pullbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pullbot.java
@@ -381,6 +381,148 @@ public class Pullbot extends GenericFTCRobot {
     }
   }
 
+  static class WobblerOrientationAnalysisPipeline extends OpenCvPipeline {
+    //   RGB colors.
+    static final Scalar RED = new Scalar(255, 0, 0);
+    static final Scalar GREEN = new Scalar(0, 255, 0);
+    static final Scalar BLUE = new Scalar(0, 0, 255);
+    //    Threshold values.
+    static final int CB_CHAN_MASK_THRESHOLD = 110;
+    static final int CONTOUR_LINE_THICKNESS = 2;
+    static final int CB_CHAN_IDX = 2;
+    //    Our working image buffers.
+    Mat crMat = new Mat();
+    Mat thresholdMat = new Mat();
+    Mat morphedThreshold = new Mat();
+    Mat contoursOnPlainImageMat = new Mat();
+    //   The elements we use for noise reduction.
+    Mat erodeElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT,
+        new Size(3, 3));
+    Mat dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT,
+        new Size(6, 6));
+    ArrayList<WobblerOrientationAnalysisPipeline.AnalyzedWobbler> internalWobblerList =
+        new ArrayList<>();
+    volatile ArrayList<WobblerOrientationAnalysisPipeline.AnalyzedWobbler> clientWobblerList = new ArrayList<>();
+    WobblerOrientationAnalysisPipeline.Stage[] stages = values();
+    //   Currently displayed stage buffer.
+    int stageNum = 0;
+
+    static void drawRotatedRect(RotatedRect rect, Mat drawOn) {
+      //   Draws a rotated rect by drawing each of the 4 lines individually.
+      Point[] points = new Point[4];
+      rect.points(points);
+
+      for (int i = 0; i < 4; ++i) {
+        Imgproc.line(drawOn, points[i], points[(i + 1) % 4], GREEN, 2);
+      }
+    }
+
+    @Override
+    public Mat processFrame(Mat input) {
+      // We'll be updating input with new data below.
+      internalWobblerList.clear();
+
+      //   Process the image.
+      for (MatOfPoint contour : findContours(input)) {
+        analyzeContour(contour, input);
+      }
+      clientWobblerList = new ArrayList<>(internalWobblerList);
+      return input;
+    }
+
+    public ArrayList<WobblerOrientationAnalysisPipeline.AnalyzedWobbler> getDetectedWobblers() {
+      return clientWobblerList;
+    }
+
+    ArrayList<MatOfPoint> findContours(Mat input) {
+      // A list we'll be using to store the contours we find.
+      ArrayList<MatOfPoint> contoursList = new ArrayList<>();
+
+      // Convert the input image to YCrCb color space, then extract the Cb
+      // channel.
+      Imgproc.cvtColor(input, crMat, Imgproc.COLOR_RGB2YCrCb);
+      Core.extractChannel(crMat, crMat, CB_CHAN_IDX);
+
+      // Threshold the Cb channel to form a mask, then run some noise reduction.
+      Imgproc.threshold(crMat, thresholdMat, CB_CHAN_MASK_THRESHOLD, 255,
+          Imgproc.THRESH_BINARY_INV);
+      morphMask(thresholdMat, morphedThreshold);
+
+      // Ok, now actually look for the contours! We only look for external
+      // contours.
+      Imgproc.findContours(morphedThreshold, contoursList, new Mat(),
+          Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+
+      // We do draw the contours we find, but not to the main input buffer.
+      input.copyTo(contoursOnPlainImageMat);
+      Imgproc.drawContours(contoursOnPlainImageMat, contoursList, -1,
+          BLUE, CONTOUR_LINE_THICKNESS, 8);
+
+      return contoursList;
+    }
+
+    void morphMask(Mat input, Mat output) {
+      //   Noise reduction.
+      Imgproc.erode(input, output, erodeElement);
+      Imgproc.erode(output, output, erodeElement);
+      Imgproc.dilate(output, output, dilateElement);
+      Imgproc.dilate(output, output, dilateElement);
+    }
+
+    void analyzeContour(MatOfPoint contour, Mat input) {
+      boolean isMaybeWobbler = true;
+      WobblerOrientationAnalysisPipeline.AnalyzedWobbler analyzedWobbler =
+          new WobblerOrientationAnalysisPipeline.AnalyzedWobbler();
+      //   Transform the contour to a different format.
+      Point[] points = contour.toArray();
+      MatOfPoint2f contour2f = new MatOfPoint2f(contour.toArray());
+
+      //   Do a rect fit to the contour, and draw it on the screen.
+      RotatedRect rotatedRectFitToContour =
+          Imgproc.minAreaRect(contour2f);
+      drawRotatedRect(rotatedRectFitToContour, input);
+      AnalyzedWobbler.width = (int) rotatedRectFitToContour.size.width;
+      AnalyzedWobbler.aspectRatio = rotatedRectFitToContour.size.width /
+          rotatedRectFitToContour.size.height;
+      AnalyzedWobbler.top = rotatedRectFitToContour.boundingRect().y;
+      AnalyzedWobbler.left = rotatedRectFitToContour.boundingRect().x;
+      AnalyzedWobbler.height = rotatedRectFitToContour.boundingRect().height;
+      //   Throw out "Wobblers" not in proper position.
+      if (AnalyzedWobbler.top < tooHigh) isMaybeWobbler = false;
+      if (AnalyzedWobbler.left > tooFarRight) isMaybeWobbler = false;
+      if (AnalyzedWobbler.width > tooWide) isMaybeWobbler = false;
+      if (AnalyzedWobbler.height > tooTall) isMaybeWobbler = false;
+      // TODO: consolidate these with filter code in CountWobblers.
+      if (isMaybeWobbler) {
+        internalWobblerList.add(analyzedWobbler);
+        // The angle OpenCV gives us can be ambiguous, so look at the shape of
+        // the rectangle to fix that.
+        double rotRectAngle = rotatedRectFitToContour.angle;
+        if (rotatedRectFitToContour.size.width < rotatedRectFitToContour.size.height) {
+          rotRectAngle += 90;
+        }
+      }
+    }
+
+    //   Pipeline processing stages. Different image buffers are available at
+    //   each one.
+    enum Stage {
+      FINAL,
+      Cb,
+      MASK,
+      MASK_NR,
+      CONTOURS
+    }
+
+    static class AnalyzedWobbler {
+      double aspectRatio;
+      int top;
+      int left;
+      int height;
+      int width;
+    }
+  }
+
   int CountRings (int viewID){
     int ringsDetected = 0;
 
